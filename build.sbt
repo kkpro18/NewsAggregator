8d34cff869e0125a18f3515b5ed60b29ad6cb1a0
Kirisan Thiruchelvam <73609371+kkpro18@users.noreply.github.com>
2024-08-09 02:50:10 +0100

    Started Scraping Headlines from Yahoo

--- build.sbt
+++ build.sbt
@@ -6,3 +6,7 @@ lazy val root = (project in file("."))
   .settings(
     name := "NewsAggregator"
   )
+
+libraryDependencies ++= Seq(
+  "org.jsoup" % "jsoup" % "1.18.1" // Adjust the version if needed
+)
--- src/main/scala/NewsScraper.scala
+++ src/main/scala/NewsScraper.scala
@@ -1,3 +1,15 @@
-object NewsScraper {
+import org.jsoup.Jsoup
 
-}
+object NewsScraper {
+  def main(args: Array[String]): Unit = {
+    val url = "https://news.yahoo.com/"
+    val doc = Jsoup.connect(url).get()
+    val headlines = doc.select("stream-item-title").eachText()
+
+    println("Latest News Headlines:")
+
+    headlines.forEach(println)
+
+
+}}
+
